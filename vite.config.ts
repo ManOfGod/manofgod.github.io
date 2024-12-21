1afcce9406f907108661050d3d0fd5c2c7a74574
ManOfGod <junelmata@gmail.com>
2024-12-22 02:16:48 +0800

    Update config file.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '',
+  base: '/manofgod-github-io/',
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
   },
   plugins: [react()],
 })
