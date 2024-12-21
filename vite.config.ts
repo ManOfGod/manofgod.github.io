b4fe50f6e2ac94a68893e70bd3e6cfbf83cf0e9b
ManOfGod <junelmata@gmail.com>
2024-12-22 02:23:58 +0800

    Test.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/manofgod-github-io/',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
   },
   plugins: [react()],
 })
